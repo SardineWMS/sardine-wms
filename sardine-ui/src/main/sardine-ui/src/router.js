affab6e234517b24b3246fc35130a7995d8ab397
zhangsai02 <674016520@qq.com>
2017-01-04 16:58:01 +0800

    注释

--- sardine-ui/src/main/sardine-ui/src/router.js
+++ sardine-ui/src/main/sardine-ui/src/router.js
@@ -23,14 +23,7 @@ export default function({
       <Route path="/users" component={Demo} />
       <Route path="/home" component={Content} />
       <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-{/*
-	 * <Route path="/demo" component={App}> <IndexRoute component={Demo} />
-	 * <Route path="/users" component={Demo} /> <Route path="/create"
-	 * component={Create} /> <Route path="/search" component={Search} /> <Route
-	 * path="/view" component={View} /> </Route>
-	 */}
-      </Route>	
+      <Route path="/register" component={Register} />	
     </Router>
   );
 }
