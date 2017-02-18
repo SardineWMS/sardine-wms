8f9755504297249d816a53c015fe19b5311051c1
zhangsai02 <674016520@qq.com>
2017-02-18 15:17:31 +0800

    controller层增加log4j日志

--- sardine-wms-web/src/main/java/com/hd123/sardine/wms/web/ControllerInterceptor.java
+++ sardine-wms-web/src/main/java/com/hd123/sardine/wms/web/ControllerInterceptor.java
@@ -14,9 +14,13 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.hd123.sardine.wms.common.utils.SerializationUtils;
 
 /**
  * Controller拦截器
@@ -29,6 +33,7 @@ import org.springframework.web.servlet.ModelAndView;
  */
 public class ControllerInterceptor implements HandlerInterceptor {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(ControllerInterceptor.class);
   private static final String TOKEN_NAME = "token";
 
   private List<String> uncheckUrls;
@@ -43,27 +48,34 @@ public class ControllerInterceptor implements HandlerInterceptor {
   }
 
   @Override
-  public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2,
-      Exception arg3) throws Exception {
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+      Object handler, Exception arg3) throws Exception {
 
   }
 
   @Override
-  public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2,
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
       ModelAndView arg3) throws Exception {
-
+    if (handler instanceof HandlerMethod) {
+      HandlerMethod method = (HandlerMethod) handler;
+      LOGGER.info(method.getBeanType().getSimpleName() + "--->" + method.getMethod().getName()
+          + "执行完成，参数：" + SerializationUtils.serialize(request.getParameterMap()));
+    }
   }
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
       throws Exception {
-    String requestUrl = request.getRequestURI();
-    if (uncheckUrls.contains(requestUrl.substring(url_prefix.length())))
-      return true;
-
     if (handler instanceof HandlerMethod) {
-      String token = request.getParameter(TOKEN_NAME);
       HandlerMethod method = (HandlerMethod) handler;
+      LOGGER.info(method.getBeanType().getSimpleName() + "--->" + method.getMethod().getName()
+          + "开始执行，参数：" + SerializationUtils.serialize(request.getParameterMap()));
+
+      String requestUrl = request.getRequestURI();
+      if (uncheckUrls.contains(requestUrl.substring(url_prefix.length())))
+        return true;
+
+      String token = request.getParameter(TOKEN_NAME);
       BaseController controller = (BaseController) method.getBean();
       controller.checkUserInfo(token);
       return true;
