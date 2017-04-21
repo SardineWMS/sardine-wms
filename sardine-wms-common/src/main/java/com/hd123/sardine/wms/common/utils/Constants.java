75a7c968633db0f24e0ff2fa15f51bc2cbf4d07b
zhangsai02 <674016520@qq.com>
2017-04-21 14:46:37 +0800

    入库服务

---
+++ sardine-wms-common/src/main/java/com/hd123/sardine/wms/common/utils/Constants.java
@@ -0,0 +1,34 @@
+/**
+ * 版权所有(C)，上海海鼎信息工程股份有限公司，2017，所有权利保留。
+ * 
+ * 项目名：	sardine-wms-common
+ * 文件名：	Constants.java
+ * 模块说明：	
+ * 修改历史：
+ * 2017年4月10日 - zhangsai - 创建。
+ */
+package com.hd123.sardine.wms.common.utils;
+
+/**
+ * 系统常量
+ * 
+ * @author zhangsai
+ *
+ */
+public class Constants {
+
+  /** 公司ID起始序号 */
+  public static final String COMPANY_ID_START = "100001";
+
+  /** 配送中心ID前缀 */
+  public static final String DC_PREFIX = "dc";
+
+  /** 供应商ID前缀 */
+  public static final String SUPP_PREFIX = "supp";
+
+  /** 承运商ID前缀 */
+  public static final String CARR_PREFIX = "carr";
+
+  /** 数据库前缀名 */
+  public static final String DB_PREFIX = "sardine";
+}
