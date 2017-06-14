eab132673f603016a75ef69ea514ed813df8be5a
zhangsai02 <674016520@qq.com>
2017-06-14 17:22:40 +0800

    指令服务

--- sardine-wms-web/src/main/java/com/hd123/sardine/wms/web/inner/task/TaskController.java
+++ sardine-wms-web/src/main/java/com/hd123/sardine/wms/web/inner/task/TaskController.java
@@ -9,7 +9,12 @@
  */
 package com.hd123.sardine.wms.web.inner.task;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +22,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hd123.rumba.commons.lang.StringUtil;
+import com.hd123.sardine.wms.api.stock.StockExtendInfo;
+import com.hd123.sardine.wms.api.stock.StockFilter;
+import com.hd123.sardine.wms.api.stock.StockService;
+import com.hd123.sardine.wms.api.task.ArticleMoveRule;
+import com.hd123.sardine.wms.api.task.ContainerMoveRule;
 import com.hd123.sardine.wms.api.task.Task;
 import com.hd123.sardine.wms.api.task.TaskService;
 import com.hd123.sardine.wms.api.task.TaskState;
@@ -40,6 +50,9 @@ public class TaskController extends BaseController {
 
   @Autowired
   private TaskService taskService;
+
+  @Autowired
+  private StockService stockService;
 
   @RequestMapping(value = "/query", method = RequestMethod.GET)
   public @ResponseBody RespObject query(
@@ -74,4 +87,115 @@ public class TaskController extends BaseController {
     return resp;
   }
 
+  @RequestMapping(value = "/queryStocks", method = RequestMethod.GET)
+  public @ResponseBody RespObject queryStocks(
+      @RequestParam(value = "token", required = true) String token,
+      @RequestParam(value = "articleCode", required = true) String articleCode) {
+    RespObject resp = new RespObject();
+
+    try {
+      StockFilter stockFilter = new StockFilter();
+      stockFilter.setArticleCode(articleCode);
+      List<StockExtendInfo> infos = stockService.queryStocks(stockFilter);
+      resp.setObj(infos);
+    } catch (Exception e) {
+      return new ErrorRespObject("分页查询失败", e.getMessage());
+    }
+    return resp;
+  }
+
+  @RequestMapping(value = "/saveArticleMoveRule", method = RequestMethod.POST)
+  public @ResponseBody RespObject saveArticleMoveRule(
+      @RequestParam(value = "token", required = true) String token,
+      @RequestBody List<ArticleMoveRule> articleMoveRules) {
+    RespObject resp = new RespObject();
+    if (CollectionUtils.isEmpty(articleMoveRules))
+      return resp;
+
+    try {
+      taskService.saveArticleMoveTask(articleMoveRules);
+    } catch (Exception e) {
+      return new ErrorRespObject("保存移库规则失败！", e.getMessage());
+    }
+    return resp;
+  }
+
+  @RequestMapping(value = "/saveAndMoveArticleMoveRule", method = RequestMethod.POST)
+  public @ResponseBody RespObject saveAndMoveArticleMoveRule(
+      @RequestParam(value = "token", required = true) String token,
+      @RequestBody List<ArticleMoveRule> articleMoveRules) {
+    RespObject resp = new RespObject();
+    if (CollectionUtils.isEmpty(articleMoveRules))
+      return resp;
+
+    try {
+      taskService.saveAndMoveArticleMoveTask(articleMoveRules);
+    } catch (Exception e) {
+      return new ErrorRespObject("保存并移库失败！", e.getMessage());
+    }
+    return resp;
+  }
+
+  @RequestMapping(value = "/saveContainerMoveRule", method = RequestMethod.POST)
+  public @ResponseBody RespObject saveContainerMoveRule(
+      @RequestParam(value = "token", required = true) String token,
+      @RequestBody List<ContainerMoveRule> containereMoveRules) {
+    RespObject resp = new RespObject();
+    if (CollectionUtils.isEmpty(containereMoveRules))
+      return resp;
+
+    try {
+      taskService.saveContainerMoveTask(containereMoveRules);
+    } catch (Exception e) {
+      return new ErrorRespObject("保存容器移库规则失败！", e.getMessage());
+    }
+    return resp;
+  }
+
+  @RequestMapping(value = "/saveAndMoveContainerMoveRule", method = RequestMethod.POST)
+  public @ResponseBody RespObject saveAndMoveContainerMoveRule(
+      @RequestParam(value = "token", required = true) String token,
+      @RequestBody List<ContainerMoveRule> containereMoveRules) {
+    RespObject resp = new RespObject();
+    if (CollectionUtils.isEmpty(containereMoveRules))
+      return resp;
+
+    try {
+      taskService.saveAndMoveContainerMoveTask(containereMoveRules);
+    } catch (Exception e) {
+      return new ErrorRespObject("保存并移库失败！", e.getMessage());
+    }
+    return resp;
+  }
+
+  @RequestMapping(value = "/articleMove", method = RequestMethod.PUT)
+  public @ResponseBody RespObject articleMove(
+      @RequestParam(value = "token", required = true) String token,
+      @RequestParam(value = "uuid", required = true) String uuid,
+      @RequestParam(value = "version", required = true) int version,
+      @RequestParam(value = "realQty", required = true) BigDecimal realQty) {
+    RespObject resp = new RespObject();
+
+    try {
+      taskService.articleMove(uuid, version, realQty);
+    } catch (Exception e) {
+      return new ErrorRespObject("保存并移库失败！", e.getMessage());
+    }
+    return resp;
+  }
+
+  @RequestMapping(value = "/containerMove", method = RequestMethod.PUT)
+  public @ResponseBody RespObject containerMove(
+      @RequestParam(value = "token", required = true) String token,
+      @RequestParam(value = "uuid", required = true) String uuid,
+      @RequestParam(value = "version", required = true) int version) {
+    RespObject resp = new RespObject();
+
+    try {
+      taskService.containerMove(uuid, version);
+    } catch (Exception e) {
+      return new ErrorRespObject("保存并移库失败！", e.getMessage());
+    }
+    return resp;
+  }
 }
