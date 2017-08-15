f8305dac9b24b92244a4c92a794e0ab6ad611e3d
fanqingqing <fqq89757@163.com>
2017-08-15 08:53:23 +0800

    Sardine-6930

--- sardine-wms-web/src/main/java/com/hd123/sardine/wms/web/basicInfo/config/pickareaconfig/PickAreaStorageAreaConfigController.java
+++ sardine-wms-web/src/main/java/com/hd123/sardine/wms/web/basicInfo/config/pickareaconfig/PickAreaStorageAreaConfigController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hd123.rumba.commons.lang.StringUtil;
 import com.hd123.sardine.wms.api.basicInfo.config.pickareastorageareaconfig.PickAreaStorageAreaConfig;
 import com.hd123.sardine.wms.api.basicInfo.config.pickareastorageareaconfig.PickAreaStorageAreaConfigService;
 import com.hd123.sardine.wms.common.http.ErrorRespObject;
@@ -33,50 +34,52 @@ import com.hd123.sardine.wms.web.base.BaseController;
 @RestController
 @RequestMapping("/basicinfo/pickareastorageareaconfig")
 public class PickAreaStorageAreaConfigController extends BaseController {
-  @Autowired
-  private PickAreaStorageAreaConfigService service;
+    @Autowired
+    private PickAreaStorageAreaConfigService service;
 
-  @RequestMapping(value = "/querybypage", method = RequestMethod.GET)
-  public @ResponseBody RespObject query(
-      @RequestParam(value = "page", required = false, defaultValue = "1") int page,
-      @RequestParam(value = "pageSize", required = false, defaultValue = "50") int pageSize,
-      @RequestParam(value = "sort", required = false) String sort,
-      @RequestParam(value = "order", required = false, defaultValue = "asc") String sortDirection,
-      @RequestParam(value = "token", required = true) String token,
-      @RequestParam(value = "pickAreaCode", required = false) String pickAreaCode,
-      @RequestParam(value = "pickAreaName", required = false) String pickAreaName) {
-    RespObject resp = new RespObject();
-    try {
-      PageQueryDefinition definition = new PageQueryDefinition();
-      definition.setPage(page);
-      definition.setPageSize(pageSize);
-      definition.setSortField(sort);
-      definition.setOrderDir(OrderDir.valueOf(sortDirection));
-      definition.put(PickAreaStorageAreaConfigService.QUERY_PICKAREACODE_FIELD, pickAreaCode);
-      definition.put(PickAreaStorageAreaConfigService.QUERY_PICKAREANAME_FIELD, pickAreaName);
-      PageQueryResult<PickAreaStorageAreaConfig> result = service.query(definition);
+    @RequestMapping(value = "/querybypage", method = RequestMethod.GET)
+    public @ResponseBody RespObject query(
+            @RequestParam(value = "page", required = false, defaultValue = "1") int page,
+            @RequestParam(value = "pageSize", required = false, defaultValue = "50") int pageSize,
+            @RequestParam(value = "sort", required = false) String sort,
+            @RequestParam(value = "order", required = false,
+                    defaultValue = "asc") String sortDirection,
+            @RequestParam(value = "token", required = true) String token,
+            @RequestParam(value = "pickAreaCode", required = false) String pickAreaCode,
+            @RequestParam(value = "pickAreaName", required = false) String pickAreaName) {
+        RespObject resp = new RespObject();
+        try {
+            PageQueryDefinition definition = new PageQueryDefinition();
+            definition.setPage(page);
+            definition.setPageSize(pageSize);
+            if (StringUtil.isNullOrBlank(sort) == false && "pickAreaCode".equals(sort))
+                definition.setSortField("code");
+            definition.setOrderDir(OrderDir.valueOf(sortDirection));
+            definition.put(PickAreaStorageAreaConfigService.QUERY_PICKAREACODE_FIELD, pickAreaCode);
+            definition.put(PickAreaStorageAreaConfigService.QUERY_PICKAREANAME_FIELD, pickAreaName);
+            PageQueryResult<PickAreaStorageAreaConfig> result = service.query(definition);
 
-      resp.setObj(result);
-      resp.setStatus(RespStatus.HTTP_STATUS_SUCCESS);
-    } catch (Exception e) {
-      return new ErrorRespObject("分页查询失败：" + e.getMessage());
+            resp.setObj(result);
+            resp.setStatus(RespStatus.HTTP_STATUS_SUCCESS);
+        } catch (Exception e) {
+            return new ErrorRespObject("分页查询失败：" + e.getMessage());
+        }
+        return resp;
     }
-    return resp;
-  }
 
-  @RequestMapping(value = "/setpickareastorageareaconfig", method = RequestMethod.PUT)
-  public @ResponseBody RespObject setPickAreaStorageAreaConfig(
-      @RequestParam(value = "token", required = true) String token,
-      @RequestParam(value = "pickAreaUuid", required = true) String pickAreaUuid,
-      @RequestParam(value = "storageArea", required = false) String storageArea,
-      @RequestParam(value = "version", required = false) long version) {
-    RespObject resp = new RespObject();
-    try {
-      service.setPickAreaStorageAreaConfig(pickAreaUuid, storageArea, version);
-      resp.setStatus(RespStatus.HTTP_STATUS_SUCCESS);
-    } catch (Exception e) {
-      return new ErrorRespObject("商品类别存储区域设置：" + e.getMessage());
+    @RequestMapping(value = "/setpickareastorageareaconfig", method = RequestMethod.PUT)
+    public @ResponseBody RespObject setPickAreaStorageAreaConfig(
+            @RequestParam(value = "token", required = true) String token,
+            @RequestParam(value = "pickAreaUuid", required = true) String pickAreaUuid,
+            @RequestParam(value = "storageArea", required = false) String storageArea,
+            @RequestParam(value = "version", required = false) long version) {
+        RespObject resp = new RespObject();
+        try {
+            service.setPickAreaStorageAreaConfig(pickAreaUuid, storageArea, version);
+            resp.setStatus(RespStatus.HTTP_STATUS_SUCCESS);
+        } catch (Exception e) {
+            return new ErrorRespObject("商品类别存储区域设置：" + e.getMessage());
+        }
+        return resp;
     }
-    return resp;
-  }
 }
