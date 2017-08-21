2cf2ba94239a626c141a69a978b3ac02b80d86a2
zhangsai02 <674016520@qq.com>
2017-08-21 17:18:10 +0800

    DAO单元测试

--- sardine-wms-dao/src/test/java/com/hd123/sardine/wms/dao/AbstractDataAccessTests.java
+++ sardine-wms-dao/src/test/java/com/hd123/sardine/wms/dao/AbstractDataAccessTests.java
@@ -9,7 +9,10 @@
  */
 package com.hd123.sardine.wms.dao;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -19,6 +22,12 @@ import org.springframework.test.context.support.DirtiesContextTestExecutionListe
 import org.springframework.transaction.annotation.Transactional;
 
 import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
+import com.hd123.sardine.wms.api.ia.user.User;
+import com.hd123.sardine.wms.common.entity.OperateContext;
+import com.hd123.sardine.wms.common.entity.Operator;
+import com.hd123.sardine.wms.common.entity.UCN;
+import com.hd123.sardine.wms.common.utils.ApplicationContextUtil;
+import com.hd123.sardine.wms.dao.ia.user.UserDao;
 
 /**
  * @author zhanglin
@@ -26,12 +35,26 @@ import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
-    "classpath:dao.xml", "classpath:applicationContext-ds-test-hsql.xml" })
+    "classpath:dao.xml", "classpath:applicationContext-ds.xml" })
 @Rollback
 @Transactional
 @TestExecutionListeners({
     DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class,
     TransactionDbUnitTestExecutionListener.class })
 public class AbstractDataAccessTests {
+  @Autowired
+  private UserDao userDao;
 
+  @Before
+  public void login() throws Exception {
+    User user = userDao.login("zhangsai", "admin8888");
+    ApplicationContextUtil.setCompany(
+        new UCN(user.getCompanyUuid(), user.getCompanyCode(), user.getCompanyName()));
+    ApplicationContextUtil.setOperateContext(new OperateContext(new Operator("zs", "zs", "zs")));
+  }
+  
+  @Test
+  public void test() {
+    userDao.get("24234");
+  }
 }
