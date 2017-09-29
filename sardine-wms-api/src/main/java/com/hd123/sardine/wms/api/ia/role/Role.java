15bd812d6dbc23cdddb619ac6cfc2d6e59e7d62b
yangwenzhu <ywz19950420@163.com>
2017-09-29 16:33:30 +0800

    ZWMS-7326

--- sardine-wms-api/src/main/java/com/hd123/sardine/wms/api/ia/role/Role.java
+++ sardine-wms-api/src/main/java/com/hd123/sardine/wms/api/ia/role/Role.java
@@ -18,92 +18,101 @@ import com.hd123.sardine.wms.common.entity.StandardEntity;
  *
  */
 public class Role extends StandardEntity {
-    private static final long serialVersionUID = 7747961149895850151L;
+  private static final long serialVersionUID = 7747961149895850151L;
 
-    private String code;
-    private String name;
-    private String companyUuid;
-    private RoleState state = RoleState.online;
+  private String code;
+  private String name;
+  private String companyUuid;
+  private RoleState state = RoleState.online;
+  private String remark;
 
-    private boolean owned;
+  private boolean owned;
 
-    public String getCompanyUuid() {
-        return companyUuid;
-    }
+  public String getCompanyUuid() {
+    return companyUuid;
+  }
 
-    public void setCompanyUuid(String companyUuid) {
-        this.companyUuid = companyUuid;
-    }
+  public void setCompanyUuid(String companyUuid) {
+    this.companyUuid = companyUuid;
+  }
 
-    public String getCode() {
-        return code;
-    }
+  public String getCode() {
+    return code;
+  }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
+  public void setCode(String code) {
+    this.code = code;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public RoleState getState() {
-        return state;
-    }
+  public RoleState getState() {
+    return state;
+  }
 
-    public void setState(RoleState state) {
-        this.state = state;
-    }
+  public void setState(RoleState state) {
+    this.state = state;
+  }
 
-    public boolean isOwned() {
-        return owned;
-    }
+  public boolean isOwned() {
+    return owned;
+  }
 
-    public void setOwned(boolean owned) {
-        this.owned = owned;
-    }
+  public void setOwned(boolean owned) {
+    this.owned = owned;
+  }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = super.hashCode();
-        result = prime * result + ((code == null) ? 0 : code.hashCode());
-        result = prime * result + ((companyUuid == null) ? 0 : companyUuid.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((state == null) ? 0 : state.hashCode());
-        return result;
-    }
+  public String getRemark() {
+    return remark;
+  }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (!super.equals(obj))
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Role other = (Role) obj;
-        if (code == null) {
-            if (other.code != null)
-                return false;
-        } else if (!code.equals(other.code))
-            return false;
-        if (companyUuid == null) {
-            if (other.companyUuid != null)
-                return false;
-        } else if (!companyUuid.equals(other.companyUuid))
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        if (state != other.state)
-            return false;
-        return true;
-    }
+  public void setRemark(String remark) {
+    this.remark = remark;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = super.hashCode();
+    result = prime * result + ((code == null) ? 0 : code.hashCode());
+    result = prime * result + ((companyUuid == null) ? 0 : companyUuid.hashCode());
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    result = prime * result + ((state == null) ? 0 : state.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (!super.equals(obj))
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Role other = (Role) obj;
+    if (code == null) {
+      if (other.code != null)
+        return false;
+    } else if (!code.equals(other.code))
+      return false;
+    if (companyUuid == null) {
+      if (other.companyUuid != null)
+        return false;
+    } else if (!companyUuid.equals(other.companyUuid))
+      return false;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    if (state != other.state)
+      return false;
+    return true;
+  }
 }
